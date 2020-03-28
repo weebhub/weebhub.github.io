f8770adf01d25d37dd02b5348ddd1d63b7aaa550
ShiroXXBlank <62727326+ShiroXXBlank@users.noreply.github.com>
2020-03-28 17:23:46 -0500

    Removed Requests
    MAD :(

--- indexJS.js
+++ indexJS.js
@@ -1,9 +1,14 @@
-function gotoWeb(websiteURL){
-    if(typeof(websiteURL) == "string") {
+function gotoWeb (websiteURL) {
+    if (typeof(websiteURL) == "string") {
         // DIDN'T WORK BECAUSE I DIDN'T RETURN ITT
         return window.location.href = websiteURL;
-    }else{
+    } else{
         console.log("Error, invalid type of url");
         return;
     }
+}
+
+function clearFields () {
+    document.getElementById("nameInput").innerHTML = "";
+    document.getElementById("commentInput").innerHTML = "";
 }

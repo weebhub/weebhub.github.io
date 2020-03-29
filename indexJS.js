14ea2547cc378cd768d58c10b28831b8445e3a5c
ShiroXXBlank <62727326+ShiroXXBlank@users.noreply.github.com>
2020-03-29 18:50:43 -0500

    Testing:

--- indexJS.js
+++ indexJS.js
@@ -10,38 +10,7 @@ function gotoWeb (websiteURL) {
     }
 }
 
-function findKey(key) {
-    // use like contains or includes name
-    if (localStorage.length >= 1) {
-        for (var i = 0; i < localStorage.length; i++) {
-            if (localStorage.key(i).search(key) != -1) {
-                return localStorage.key(i);
-            }
-        }
-    }else{
-        return key;
-    }
-}
-
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
- }
-
-function replaceChar(origString, replaceChar, index) {
-    let firstPart = origString.substr(0, index);
-    let lastPart = origString.substr(index + 1);
-      
-    let newString = firstPart + replaceChar + lastPart;
-    return newString;
-}
 
 function clearFields () {
-    document.getElementById("nameInput").innerHTML = '';
     document.getElementById("commentInput").innerHTML = '';
 }

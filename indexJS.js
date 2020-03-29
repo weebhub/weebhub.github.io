5603c50db6bc1651904f0e2860b4063f7e5c5bd6
ShiroXXBlank <62727326+ShiroXXBlank@users.noreply.github.com>
2020-03-28 20:56:28 -0500

    Added requests aganee
    hopefully local storage is shared with everyone

--- indexJS.js
+++ indexJS.js
@@ -1,3 +1,5 @@
+let obj;
+
 function gotoWeb (websiteURL) {
     if (typeof(websiteURL) == "string") {
         // DIDN'T WORK BECAUSE I DIDN'T RETURN ITT
@@ -8,7 +10,55 @@ function gotoWeb (websiteURL) {
     }
 }
 
+function findKey(key) {
+    // use like contains or includes name
+    if (localStorage.length >= 1) {
+        for (var i = 0; i < localStorage.length; i++) {
+            if (localStorage.key(i).search(key) != -1) {
+                return localStorage.key(i);
+            }
+        }
+    }else{
+        return key;
+    }
+}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+
+function replaceChar(origString, replaceChar, index) {
+    let firstPart = origString.substr(0, index);
+    let lastPart = origString.substr(index + 1);
+      
+    let newString = firstPart + replaceChar + lastPart;
+    return newString;
+}
+
 function clearFields () {
-    document.getElementById("nameInput").innerHTML = "";
-    document.getElementById("commentInput").innerHTML = "";
+    document.getElementById("nameInput").value = '';
+    document.getElementById("commentInput").value = "";
+}
+
+function setLocalStorage () {
+    var comments = document.getElementById("commentInput").value;
+
+    var inc = makeid(20);
+
+    if (comments) {
+        obj = localStorage.setItem(inc, comments);
+
+        alert("Thank you for the input!! <3");
+        clearFields();
+        return;
+    }else {
+        alert("You have to add stuff to your fields!");
+        return;
+    }
 }
